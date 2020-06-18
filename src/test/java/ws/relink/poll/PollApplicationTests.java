5cb5999c576f8151383b05c682dc702da009334b
Sebastião Relson <relsonto@gmail.com>
2020-06-17 22:44:10 -0300

    springboot init

---
+++ src/test/java/ws/relink/poll/PollApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.relink.poll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
